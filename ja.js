5c844ea76f6f3e174e2914df42c468b6e8ac35e5
sasha3000f <166547755+sasha3000f@users.noreply.github.com>
2025-01-02 11:45:58 +0200

    Add files via upload

--- ja.js
+++ ja.js
@@ -1,14 +1,14 @@
-var pisun = 0;
+var het = 0;
 here
 function func1() {
-  pisun += 1;
+  het += 1;
   document.getElementById('het').innerHTML = het;
 };
 function func2() {
-  pisun += -1;
+  het += -1;
   document.getElementById('het').innerHTML = het;
 };
 function func3() {
-  pisun -= pisun;
+  het -= het;
   document.getElementById('het').innerHTML = het;
 }
